b267ef68afececa7fe2068b21ef9d0cfcbaee973
sandeep <sandeepphalaswar@gmail.com>
2023-09-20 21:06:22 +0530

    india
    test

--- quiz.js
+++ quiz.js
@@ -1,4 +1,3 @@
-// quiz.js
 const tigerReserves = [
   "Bandipur Tiger Reserve",
   "Jim Corbett Tiger Reserve",
@@ -10,22 +9,26 @@ const tigerReserves = [
 ];
 
 let currentQuestionIndex = 0;
+let correctAttemptsFirstTry = 0;
 
 function startQuiz() {
   currentQuestionIndex = 0;
+  correctAttemptsFirstTry = 0;
   showQuestion();
-  document.getElementById("result").innerText = "";
+  document.getElementById("result-message").innerText = "";
+  document.querySelector(".result-section").classList.add("hidden");
 }
 
 function showQuestion() {
-  document.getElementById("question").innerText = "Guess the Tiger Reserve:";
+  document.getElementById("question").innerText = "Select on map:";
   document.getElementById("reserve-name").innerText = tigerReserves[currentQuestionIndex];
 }
 
 function checkAnswer(selectedButtonIndex) {
   const correctButtonIndex = currentQuestionIndex + 1;
 
-  if (selectedButtonIndex === correctButtonIndex) {
+  if (selectedButtonIndex === correctButtonIndex && correctAttemptsFirstTry === currentQuestionIndex) {
+    correctAttemptsFirstTry++;
     const buttons = document.querySelectorAll(".map-button");
     buttons[currentQuestionIndex].classList.add("correct");
     buttons[currentQuestionIndex].innerText = tigerReserves[currentQuestionIndex];
@@ -52,23 +55,26 @@ function nextQuestion() {
   } else {
     document.getElementById("question").innerText = "Congratulations! You have completed the quiz.";
     document.getElementById("reserve-name").innerText = "";
-     displayResult();
-  } 
+    displayResult();
+  }
 }
 
 function displayResult() {
-    const resultPercentage = (correctAttempts / totalAttempts) * 100;
-    const resultMessage = `You guessed the right button ${correctAttempts} times out of ${totalAttempts}. Your success rate is ${resultPercentage.toFixed(2)}%.`;
-    document.getElementById("result").innerText = resultMessage;
+  const resultMessage = `You guessed ${correctAttemptsFirstTry} questions right on the first attempt out of ${tigerReserves.length}. Your score is ${resultPercentage.toFixed(2)}%.`;
+  document.getElementById("result-message").innerText = resultMessage;
+  document.querySelector(".result-section").classList.remove("hidden");
 }
+
+
+// Rest of the code remains unchanged
+
 // Reset the quiz when the page is loaded or reloaded
 window.onload = () => {
   startQuiz();
   createButtons();
-  setButtonCoordinates(); // Set initial button coordinates
+  setButtonCoordinates();
 
-  // Add a window resize event listener to update button coordinates on window resize
-  window.addEventListener('resize', setButtonCoordinates);
+  window.addEventListener("resize", setButtonCoordinates);
 };
 
 // Function to create buttons for the quiz
@@ -80,53 +86,50 @@ function createButtons() {
     button.onclick = () => checkAnswer(i + 1);
     buttonsOverlay.appendChild(button);
 
-    // Add event listener to play click sound on click
-    button.addEventListener('click', playClickSound);
+    button.addEventListener("click", playClickSound);
   }
 }
 
 // Function to set new coordinates for the buttons
 function setButtonCoordinates() {
-  // Adjust the button coordinates in pixels here as needed for different screen sizes
   if (window.innerWidth <= 1000) {
-    // Coordinates for mobile devices
     const buttonCoordinatesMobile = [
-      { top: '300px', left: '90px' }, // Button 1
-      { top: '85px', left: '135px' }, // Button 2
-      { top: '300px', left: '130px' }, // Button 3
-      { top: '180px', left: '120px' }, // Button 4
-      { top: '150px', left: '70px' }, // Button 5
-      { top: '190px', left: '220px' }, // Button 6
-      { top: '340px', left: '95px' }, // Button 7
+      { top: "350px", left: "100px" }, // Button 1
+      { top: "105px", left: "155px" }, // Button 2
+      { top: "330px", left: "150px" }, // Button 3
+      { top: "210px", left: "130px" }, // Button 4
+      { top: "190px", left: "80px" }, // Button 5
+      { top: "210px", left: "270px" }, // Button 6
+      { top: "400px", left: "110px" }, // Button 7
     ];
 
-    const buttons = document.querySelectorAll('.map-button');
+    const buttons = document.querySelectorAll(".map-button");
     buttons.forEach((button, index) => {
       button.style.top = buttonCoordinatesMobile[index].top;
       button.style.left = buttonCoordinatesMobile[index].left;
     });
-  } else {
-    // Coordinates for larger screens
-    const buttonCoordinates = [
-      { top: '620px', left: '171px' }, // Button 1
-      { top: '193px', left: '250px' }, // Button 2
-      { top: '570px', left: '250px' }, // Button 3
-      { top: '370px', left: '200px' }, // Button 4
-      { top: '330px', left: '129px' }, // Button 5
-      { top: '390px', left: '460px' }, // Button 6
-      { top: '700px', left: '190px' }, // Button 7
-    ];
-
-    const buttons = document.querySelectorAll('.map-button');
-    buttons.forEach((button, index) => {
-      button.style.top = buttonCoordinates[index].top;
-      button.style.left = buttonCoordinates[index].left;
-    });
+    } else {
+      const buttonCoordinates = [
+        { top: "620px", left: "191px" }, // Button 1
+        { top: "189px", left: "270px" }, // Button 2
+        { top: "590px", left: "250px" }, // Button 3
+        { top: "370px", left: "200px" }, // Button 4
+        { top: "330px", left: "129px" }, // Button 5
+        { top: "390px", left: "460px" }, // Button 6
+        { top: "700px", left: "204px" }, // Button 7
+      ];
+  
+      const buttons = document.querySelectorAll(".map-button");
+      buttons.forEach((button, index) => {
+        button.style.top = buttonCoordinates[index].top;
+        button.style.left = buttonCoordinates[index].left;
+      });
+    }
   }
-}
-
-// Function to play the click sound whenever a button is clicked
-function playClickSound() {
-  const clickSound = new Audio('click.mp3');
-  clickSound.play();
-}
+  
+  // Function to play the click sound whenever a button is clicked
+  function playClickSound() {
+    const clickSound = new Audio('click.mp3');
+    clickSound.play();
+  }
+  
